023fce9c1bcd3eae4e9b2802f1adaccb48ba50c6
yermtat <yerm.tat@gmail.com>
2024-07-01 21:41:12 +0400

    hw3 wip

--- homework03/src/WeatherReport.jsx
+++ homework03/src/WeatherReport.jsx
@@ -1,25 +1,58 @@
 import React, { useRef } from "react";
 import { useState } from "react";
+import Sun from "./sun.png";
+import Moon from "./moon.png";
 
 export default function WeatherReport() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
   const city = useRef();
+  const [sunTime, setSunTime] = useState({});
+
+  async function weatherFetch() {
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city.current.value}&appid=7e1dedf994df2c6df432cfb1daf1a65c&units=metric`,
+        {
+          method: "GET",
+        }
+      );
+
+      if (response.ok) {
+        await response.json().then((data) => {
+          setData(data);
+
+          setSunTime({
+            sunriseTime: new Date(data.sys.sunrise * 1000).toLocaleTimeString(),
+            sunsetTime: new Date(data.sys.sunset * 1000).toLocaleTimeString(),
+            date: new Date(data.dt * 1000).toLocaleDateString(),
+            currentTime: new Date(
+              Date.now() - (14400 - data.timezone) * 1000
+            ).toLocaleTimeString(),
+          });
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   function handleClick() {
-    console.log(city.current.value);
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city.current.value}&appid=7e1dedf994df2c6df432cfb1daf1a65c&units=metric`
-    ).then((res) => {
-      res.json().then((data) => {
-        setData(data);
-      });
-    });
-    console.log(data);
+    weatherFetch();
+  }
+
+  function renderImage() {
+    const cityTime = Date.now() / 1000 - 14400 + data.timezone;
+
+    if (cityTime > data.sys.sunrise && cityTime < data.sys.sunset) {
+      return <img src={Sun} />;
+    } else if (cityTime < data.sys.sunrise || cityTime > data.sys.sunset) {
+      return <img src={Moon} />;
+    }
   }
 
   return (
     <div>
-      <div className="w-screen h-screen m-10">
+      <div className=" flex justify-center m-10">
         <div className="w-1/2">
           <input
             id="city"
@@ -39,6 +72,78 @@ export default function WeatherReport() {
           </button>
         </div>
       </div>
+
+      {data && (
+        <div className="min-h-screen flex justify-center m-10">
+          <div className="flex flex-col bg-white rounded p-4 w-full max-w-xs">
+            <div className="font-bold text-xl">{data.name}</div>
+            <div className="text-sm text-gray-500">{sunTime.date}</div>
+            <div className="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
+              {renderImage()}
+            </div>
+            <div className="flex flex-row items-center justify-center mt-6">
+              <div className="font-medium text-6xl">{data.main.temp}°C</div>
+              <div className="flex flex-col items-center ml-6">
+                <div>{data.weather[0].main}</div>
+                <div className="mt-1">
+                  <span className="text-sm">
+                    <i className="far fa-long-arrow-up"></i>
+                  </span>
+                  <span className="text-sm font-light text-gray-500">
+                    {data.main.temp_max}°C
+                  </span>
+                </div>
+                <div>
+                  <span className="text-sm">
+                    <i className="far fa-long-arrow-down"></i>
+                  </span>
+                  <span className="text-sm font-light text-gray-500">
+                    {data.main.temp_min}°C
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between mt-6">
+              <div className="flex flex-col items-center">
+                <div className="font-medium text-sm">Wind</div>
+                <div className="text-sm text-gray-500">
+                  {data.wind.speed} k/h
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <div className="font-medium text-sm">Humidity</div>
+                <div className="text-sm text-gray-500">
+                  {data.main.humidity}%
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <div className="font-medium text-sm">Visibility</div>
+                <div className="text-sm text-gray-500">{data.visibility} m</div>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between mt-6">
+              <div className="flex flex-col items-center">
+                <div className="font-medium text-sm">Sunrise</div>
+                <div className="text-sm text-gray-500">
+                  {sunTime.sunriseTime}
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <div className="font-medium text-sm">Sunset</div>
+                <div className="text-sm text-gray-500">
+                  {sunTime.sunsetTime}
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <div className="font-medium text-sm">Current time</div>
+                <div className="text-sm text-gray-500">
+                  {sunTime.currentTime}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
